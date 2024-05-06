f21bfd48e50dd115b5c58942d1638eba23d87dde
Daniel <danielsanchezgonzalez@alumno.ieselrincon.es>
2024-05-06 19:18:18 +0100

    'multiplication.js' is now on a html file

--- multiplication.js
+++
@@ -1,9 +0,0 @@
-function multiplication(x, y = 10) {
-    return x * y;
-  }
-
-console.log(multiplication(5));
-
-console.log("\nYou can redefine the 'y' in the parameters\n");
-
-console.log(multiplication(5, 4));
