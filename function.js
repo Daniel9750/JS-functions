3f7d5e7a254a360cd2a9b7c48788b1548bb204f3
Daniel <danielsanchezgonzalez@alumno.ieselrincon.es>
2024-05-06 16:41:30 +0100

    Added a 'multiplication' function

---
+++ function.js
@@ -0,0 +1,9 @@
+function multiplication(x, y = 10) {
+    return x * y;
+  }
+
+console.log(multiplication(5));
+
+console.log("\nYou can redefine the 'y' in the parameters\n");
+
+console.log(multiplication(5, 4));
